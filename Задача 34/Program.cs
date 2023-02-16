4de2853b9536b4f61091158f766e206151c242cb
MikhailPecherkin <m.pechiorkin@yandex.ru>
2023-02-16 18:35:37 +0300

    Add 34

---
+++ Задача 34/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+System.Console.Write("ВВедите количество чисел в массиве: ");
+int size = System.Convert.ToInt32(Console.ReadLine());
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int FindQ(int[] array)
+
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int [] myArray = GenerateArray(size, 100, 1000);
+PrintArray (myArray);
+System.Console.WriteLine($"количество чётных чисел в массиве {FindQ(myArray)} ");
