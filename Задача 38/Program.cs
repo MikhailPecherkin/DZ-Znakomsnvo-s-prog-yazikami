685f81b9e3864851d487282e5b973e00e396c7cb
MikhailPecherkin <m.pechiorkin@yandex.ru>
2023-02-20 19:59:19 +0300

    Add 41 & 43

--- Задача 38/Program.cs
+++ Задача 38/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+System.Console.Write("Введите количество чисел в массиве: ");
+int size = System.Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Задайте минимальную границу: ");
+int minValue = System.Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Задайте максимальную границу: ");
+int maxValue = System.Convert.ToInt32(Console.ReadLine());
+
+double [] GenerateArray(int size, int minValue, int maxValue)
+{
+    double[] array = new double[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Math.Round(rand.Next(minValue, maxValue) + rand.NextDouble(), 2);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+double [] myArray = GenerateArray(size, minValue, maxValue);
+PrintArray (myArray);
+
+// Разница
+double min = myArray[0];
+double max = myArray[0];
+for (int i = 1; i < size; i++)
+{
+       if (myArray[i] < min)
+            min = myArray[i];
+       if (myArray[i] > max)
+            max = myArray[i];
+}
+Console.WriteLine($"Разница между минимальным значением {min} и максимальным {max} составляет: {max - min}");
---
+++ Задача 43/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.Write("Введите b1: ");
+double b1 = System.Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите k1: ");
+double k1 = System.Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите b2: ");
+double b2 = System.Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите k2: ");
+double k2 = System.Convert.ToInt32(Console.ReadLine());
+
+double x = Math.Round((b2 - b1) / (k1 -k2), 2);
+double y = Math.Round(k1 * x +b1, 2);
+
+
+System.Console.WriteLine($"Точка пересечения двух прямых: ({x}; {y})");
