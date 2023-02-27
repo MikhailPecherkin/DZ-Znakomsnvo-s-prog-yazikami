5be09d72a8e374d669078b9fa4bdc45e2ab00b8e
MikhailPecherkin <m.pechiorkin@yandex.ru>
2023-02-27 17:48:44 +0300

    Correct 54

--- Задача 54/Program.cs
+++ Задача 54/Program.cs
@@ -42,31 +42,35 @@ void PrintMatrix(int[,] matrix)
         System.Console.WriteLine();
     }
 }
-var myMatrix = GenerateMatrix(rows, cols);
-PrintMatrix(myMatrix);
-System.Console.WriteLine("----------------------------------------");
-
-System.Console.WriteLine("По убыванию в строке");
-int[,]? copy = myMatrix.Clone() as int[,];
-for (int i = 0; i < copy?.GetLength(0); i++)
+void Copysort(int[,] matrix)
 {
-    for (int j = 0; j < copy.GetLength(1); j++)
+    int[,]? copy = matrix.Clone() as int[,];
+    for (int i = 0; i < copy?.GetLength(0); i++)
     {
-        for (int k = 0; k < copy.GetLength(1); k++)
+        for (int j = 0; j < copy.GetLength(1); j++)
         {
-            if (copy[i, j] <= copy[i, k])
-                continue;
-            else
+            for (int k = 0; k < copy.GetLength(1); k++)
             {
-                int temp = copy[i, j];
-                copy[i, j] = copy[i, k];
-                copy[i, k] = temp;
+                if (copy[i, j] <= copy[i, k])
+                    continue;
+                else
+                {
+                    int temp = copy[i, j];
+                    copy[i, j] = copy[i, k];
+                    copy[i, k] = temp;
+                }
             }
         }
     }
+    for (int i = 0; i < copy?.GetLength(0); i++, Console.WriteLine())
+        for (int j = 0; j < copy.GetLength(1); j++)
+        {
+            Console.Write(copy[i, j] + "\t");
+        }
 }
-for (int i = 0; i < copy?.GetLength(0); i++, Console.WriteLine())
-    for (int j = 0; j < copy.GetLength(1); j++)
-    {
-        Console.Write(copy[i, j] + "\t");
-    }
+
+var myMatrix = GenerateMatrix(rows, cols);
+PrintMatrix(myMatrix);
+System.Console.WriteLine("----------------------------------------");
+System.Console.WriteLine("По убыванию в строке");
+Copysort(myMatrix);
