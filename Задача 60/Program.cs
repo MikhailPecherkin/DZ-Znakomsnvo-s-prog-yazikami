077123a5ba1dccc600c9fb56467b692e777b8fa5
MikhailPecherkin <m.pechiorkin@yandex.ru>
2023-03-08 21:13:08 +0300

    Add 60

--- Задача 60/Program.cs
+++ Задача 60/Program.cs
@@ -6,21 +6,36 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
-int rows = 2;
-int cols = 2;
-int dep = 2;
 int[,,] GenerateMatrix(int rows, int cols, int dep)
 {
     int[,,] matrix = new int[rows, cols, dep];
-    var rand = new Random();
+    int[] temp = new int[matrix.GetLength(0) * matrix.GetLength(1) * matrix.GetLength(2)];
+    for (int i = 0; i < matrix.GetLength(0) * matrix.GetLength(1) * matrix.GetLength(2); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        int number = temp[i];
+        
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+    }
+    int count = 0;
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-           for (int k = 0; k < matrix.GetLength(2); k++)
-           {
-            matrix[i, j, k] = rand.Next(10, 100);
-           }
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = temp[count];
+                count++;
+            }
         }
     }
     return matrix;
@@ -33,11 +48,18 @@ void PrintMatrix(int[,,] matrix)
         {
             for (int k = 0; k < matrix.GetLength(2); k++)
             {
-                Console.Write($"{matrix[i, j, k]}({i},{j},{k})" + "\t");              
+                Console.Write($"{matrix[i, j, k]}({i},{j},{k})" + "\t");
             }
+            Console.WriteLine();
         }
-        System.Console.WriteLine();
+        Console.WriteLine();
     }
 }
+
+int rows = 2;
+int cols = 2;
+int dep = 2;
+
+GenerateMatrix(rows, cols, dep);
 var myMatrix = GenerateMatrix(rows, cols, dep);
 PrintMatrix(myMatrix);
