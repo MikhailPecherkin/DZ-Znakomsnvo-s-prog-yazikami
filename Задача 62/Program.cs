cb93da9147e5551fab6f2c9aad3a1bfb5901acab
MikhailPecherkin <m.pechiorkin@yandex.ru>
2023-03-16 11:07:27 +0300

    Add 66, 62, 68

---
+++ Задача 62/Program.cs
@@ -0,0 +1,93 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int row = ReadInt("Укажите количество строк:    ");
+int col = ReadInt("Укажите количество столбцов: ");
+
+int[,] GenerateMatrix(int row, int col)
+{
+    int[,] matrix = new int[row, col];
+    WriteElements(matrix, 0, 0, 1, 1);
+    return matrix;
+}
+
+void WriteElements(int[,] matrix, int row, int col, int count, int direction)
+// Директион- направление: 1 - направо, 2 - вниз, 3 - налево, 4 - вверх
+{
+    if (count <= matrix.GetLength(0) * matrix.GetLength(1))
+    {
+        if (direction == 1)
+        {
+            if (col < matrix.GetLength(1) && matrix[row, col] == 0)
+            {
+                matrix[row, col] = count;
+                WriteElements(matrix, row, col + 1, count + 1, 1);
+            }
+            else
+            {
+                WriteElements(matrix, row + 1, col - 1, count, 2);
+            }
+        }
+        else if (direction == 2)
+        {
+            if (row < matrix.GetLength(0) && matrix[row, col] == 0)
+            {
+                matrix[row, col] = count;
+                WriteElements(matrix, row + 1, col, count + 1, 2);
+            }
+            else
+            {
+                WriteElements(matrix, row - 1, col - 1, count, 3);
+            }
+        }
+        else if (direction == 3)
+        {
+            if (col >= 0 && matrix[row, col] == 0)
+            {
+                matrix[row, col] = count;
+                WriteElements(matrix, row, col - 1, count + 1, 3);
+            }
+            else
+            {
+                WriteElements(matrix, row - 1, col + 1, count, 4);
+            }
+        }
+        else
+        {
+            if (row >= 0 && matrix[row, col] == 0)
+            {
+                matrix[row, col] = count;
+                WriteElements(matrix, row - 1, col, count + 1, 4);
+            }
+            else
+            {
+                WriteElements(matrix, row + 1, col + 1, count, 1);
+            }
+        }
+    }
+}
+////// Блок печати
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+GenerateMatrix(row, col);
+var myMatrix = GenerateMatrix(row, col);
+PrintMatrix(myMatrix);
---
+++ Задача 68/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int m = ReadInt("Введите число М: ");
+int n = ReadInt("Введите число N: ");
+
+int Akker(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return m + 1;
+
+    }
+    else if ((m > 0) && (n == 0))
+    {
+
+        return Akker(m - 1, n);
+
+    }
+    else if ((n != 0) && (m == 0))
+    {
+
+        return Akker(n - 1, m);
+
+    }
+    else if ((m != 0) && (n > 0))
+    {
+
+        return Akker(m - 1, Akker(m, n - 1));
+
+    }
+    else if ((n > 0) && (m > 0))
+    {
+
+        return Akker(n - 1, Akker(n, m - 1));
+
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+// int Akker(int m, int n)
+// {
+//     if (!m && n)
+//      return (n + 1);
+//      else
+//      if (m and!n)
+//      return Akk(m - 1, 1);
+// return Akk(m - 1, Akk(m, n - 1));
+// }
+
+System.Console.WriteLine($"Функция Аккермана = {Akker(m, n)}");
